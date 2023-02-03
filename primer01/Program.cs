9a5a108efb4de7d1216fff925a6e8fe07665d868
UBaronRU <ubaron@mail.ru>
2023-02-03 12:45:06 +0300

    primer02 is down

---
+++ primer01/Program.cs
@@ -0,0 +1,53 @@
+﻿
+/*
+## primer01
+Показать двумерный массив размером m×n заполненный вещественными числами
+*/
+void printmas(int[,] arr)
+{
+int rows = arr.GetUpperBound(0) + 1;    // количество строк
+int columns = arr.Length / rows;        // количество столбцов
+ 
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{arr[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+}
+void fillmas(int[,] arr)
+{
+int rows = arr.GetUpperBound(0) + 1;    // количество строк
+int columns = arr.Length / rows;        // количество столбцов
+ 
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        arr[i, j] = new Random().Next(-100,+100);
+    }
+   }
+}
+void copymas(int[,] arr,int[,] arr1)
+{
+int rows = arr.GetUpperBound(0) + 1;    // количество строк
+int columns = arr.Length / rows;        // количество столбцов
+ 
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        arr1[i, j] = arr[i, j];
+    }
+   }
+}
+
+int rows = new Random().Next(5,10);    // количество строк
+int columns = new Random().Next(5,10); 
+int[,] numbers = new int [rows,columns];
+int[,] numbers2 = new int [rows,columns];
+fillmas(numbers);
+copymas(numbers,numbers2);
+printmas(numbers2);
---
+++ primer02/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+## primer02
+Задать двумерный массив следующим правилом: Aₘₙ = m+n
+*/
+void printmas(int[,] arr)
+{
+int rows = arr.GetUpperBound(0) + 1;    // количество строк
+int columns = arr.Length / rows;        // количество столбцов
+ 
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{arr[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+}
+void fillmas(int[,] arr)
+{
+int rows = arr.GetUpperBound(0) + 1;    // количество строк
+int columns = arr.Length / rows;        // количество столбцов
+ 
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        arr[i, j] = i+j;
+    }
+   }
+}
+void copymas(int[,] arr,int[,] arr1)
+{
+int rows = arr.GetUpperBound(0) + 1;    // количество строк
+int columns = arr.Length / rows;        // количество столбцов
+ 
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        arr1[i, j] = arr[i, j];
+    }
+   }
+}
+
+int rows = new Random().Next(5,10);    // количество строк
+int columns = new Random().Next(5,10); 
+int[,] numbers = new int [rows,columns];
+int[,] numbers2 = new int [rows,columns];
+fillmas(numbers);
+copymas(numbers,numbers2);
+printmas(numbers2);
